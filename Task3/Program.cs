19ddba0023e351f5aa26e35ebc05e75552e2e632
Tatyana Abramova <tanusha-atv@mail.ru>
2022-07-11 08:28:48 +0500

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Найдите разницу между максимальным и минимальным элементами массива.
+
+Console.Write("n: ");
+   
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+   
+Random rand = new Random();
+for(int i=0; i<array.Length; i++) 
+{
+  array[i] =rand.Next()%100-50;
+}
+   
+Console.Write("Массив: ");
+for(int i=0; i<array.Length; i++) 
+{
+  Console.Write(array[i] +" ");
+}
+Console.WriteLine(" ");
+   
+int imin=0, imax=0;
+   
+for(int i=0; i<array.Length; i++) 
+{
+  if(array[i] >array[imax]) imax=i;
+  if(array[i] <array[imin]) imin=i;
+}
+Console.WriteLine("min : array["+imin+"] = "+array[imin]);
+Console.WriteLine("max : array["+imax+"] = "+array[imax]);
+   
+Console.WriteLine($"max - min = {array[imax] -array[imin]}");
